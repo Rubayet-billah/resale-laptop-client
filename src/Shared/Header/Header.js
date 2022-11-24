842e209339699812a1f33e2569ba52331bb8780a
Rubayet-billah <rubayetbillah909@gmail.com>
2022-11-24 19:58:01 +0600

    logout implemented

--- src/Shared/Header/Header.js
+++ src/Shared/Header/Header.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../contexts/AuthProvider/AuthProvider';
 
 const Header = () => {
+    const { user, logoutUser } = useContext(AuthContext);
+
+    const handleLogout = () => {
+        logoutUser()
+            .then(() => { })
+            .catch(err => console.error(err))
+    }
 
     const menu = <>
         <li><Link to='/'>Home</Link></li>
         <li><Link to='/shop'>Shop</Link></li>
-        <li><Link to='/login'>Login</Link></li>
-        <li><Link to='/register'>Register</Link></li>
+        {
+            user?.uid ? <>
+
+            </> : <>
+                <li><Link to='/login'>Login</Link></li>
+                <li><Link to='/register'>Register</Link></li></>
+        }
     </>
     return (
         <div>
@@ -29,7 +42,9 @@ const Header = () => {
                     </ul>
                 </div>
                 <div className="navbar-end">
-
+                    {
+                        user?.uid ? <button onClick={handleLogout} className='btn btn-sm btn-accent btn-outline'>Logout</button> : <></>
+                    }
                 </div>
             </div>
         </div>
