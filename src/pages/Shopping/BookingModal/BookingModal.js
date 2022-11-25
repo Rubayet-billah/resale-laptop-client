5e382ad889bd10569ea9a678cce180fafb384864
Rubayet-billah <rubayetbillah909@gmail.com>
2022-11-25 18:07:53 +0600

    booking object created

--- src/pages/Shopping/BookingModal/BookingModal.js
+++ src/pages/Shopping/BookingModal/BookingModal.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AuthContext } from '../../../contexts/AuthProvider/AuthProvider';
 
 const BookingModal = ({ bookedProduct }) => {
     const { image, name, resalePrice, originalPrice, usingTime, location, seller, description, condition } = bookedProduct;
+    const { user } = useContext(AuthContext);
+
+    const handleBooking = (event) => {
+        event.preventDefault();
+        const form = event.target;
+        const phone = form.phone.value;
+        const meetingLocation = form.meetingLocation.value;
+        const booking = {
+            buyer: user.displayName,
+            email: user.email,
+            productName: name,
+            phone,
+            meetingLocation,
+
+        }
+        console.log(booking)
+    }
     return (
         <div>
             {/* The button to open modal */}
@@ -12,14 +30,17 @@ const BookingModal = ({ bookedProduct }) => {
             <div className="modal">
                 <div className="modal-box relative">
                     <label htmlFor="booking-modal" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
-                    <h3 className="text-lg font-bold">{resalePrice}</h3>
-                    <p className="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
-                    <input type="text" placeholder="Type here" className="input input-bordered w-full my-1" />
-                    <input type="text" placeholder="Type here" className="input input-bordered w-full my-1" />
-                    <input type="text" placeholder="Type here" className="input input-bordered w-full my-1" />
-                    <input type="text" placeholder="Type here" className="input input-bordered w-full my-1" />
-                    <input type="text" placeholder="Type here" className="input input-bordered w-full my-1" />
-                    <input type="text" placeholder="Type here" className="input input-bordered w-full my-1" />
+                    <h3 className="text-lg font-bold">{name}</h3>
+                    <p>Price: <span className='font-bold text-primary'>${resalePrice}</span></p>
+                    <hr />
+                    <p>User Info</p>
+                    <form onSubmit={handleBooking}>
+                        <input type="text" defaultValue={user?.displayName} className="input input-bordered w-full my-1" disabled />
+                        <input type="text" defaultValue={user?.email} className="input input-bordered w-full my-1" disabled />
+                        <input type="text" placeholder="Contact Number" name='phone' className="input input-bordered w-full my-1" required />
+                        <input type="text" placeholder="Meeting Location" name='meetingLocation' className="input input-bordered w-full my-1" required />
+                        <input type="submit" className='btn btn-primary w-full my-1' value="Confirm Booking" required />
+                    </form>
                 </div>
             </div>
         </div>
