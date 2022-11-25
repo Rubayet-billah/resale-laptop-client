64ca57f2387e99811ccfeb92fd55774757d2277b
Rubayet-billah <rubayetbillah909@gmail.com>
2022-11-26 05:06:20 +0600

    errorpage edited

--- src/pages/ErrorPage/ErrorPage.js
+++ src/pages/ErrorPage/ErrorPage.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { useRouteError } from 'react-router-dom';
 
 const ErrorPage = () => {
+    const error = useRouteError();
+    console.log(error)
     return (
-        <div>
-            <h1 className="text-5xl">An error occured</h1>
+        <div className='text-center mt-8 md:mt-40'>
+            <h1 className='text-red-500 font-bold text-4xl md:text-8xl'>Opps !</h1>
+            <p className='text-xl my-3'>An unexpected error occured :( </p>
+            <p><i>{error.status}</i> || <i>{error.statusText}</i></p>
         </div>
     );
 };
