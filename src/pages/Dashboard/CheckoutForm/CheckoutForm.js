8a1b29330739e5dde919e82036776ebd9d7be629
Rubayet-billah <rubayetbillah909@gmail.com>
2022-11-27 22:46:25 +0600

    payment method implemented

--- src/pages/Dashboard/CheckoutForm/CheckoutForm.js
+++ src/pages/Dashboard/CheckoutForm/CheckoutForm.js
@@ -1,9 +1,104 @@
-import React from 'react';
+import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
+import React, { useEffect, useState } from 'react';
 
-const CheckoutForm = () => {
+const CheckoutForm = ({ booking }) => {
+    const { buyer, email, price } = booking;
+    const [cardError, setCardError] = useState('')
+    const [cardSuccess, setCardSuccess] = useState('')
+    const [transectionId, setTransectionId] = useState('')
+    const [processing, setProcessing] = useState(false)
+    const [clientSecret, setClientSecret] = useState('')
+    const stripe = useStripe();
+    const elements = useElements();
+
+    useEffect(() => {
+        setProcessing(true)
+        fetch(`http://localhost:5000/create-payment-intent`, {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify({ price })
+        }).then(res => res.json())
+            .then(data => {
+                setClientSecret(data.clientSecret);
+            })
+    }, [price])
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        if (!stripe || !elements) {
+            return;
+        }
+
+        // get card information
+        const card = elements.getElement(CardElement);
+        if (card === null) {
+            return;
+        }
+
+        const { error, paymentMethod } = await stripe.createPaymentMethod({
+            type: 'card',
+            card
+        })
+
+        if (error) {
+            setCardError(error.message);
+        } else {
+            setCardError('');
+        }
+
+        const { paymentIntent, error: confirmError } = await stripe.confirmCardPayment(
+            clientSecret,
+            {
+                payment_method: {
+                    card: card,
+                    billing_details: {
+                        name: buyer,
+                        email: email
+                    },
+                },
+            },
+        );
+        if (confirmError) {
+            setCardError(confirmError.message);
+            return;
+        }
+        if (paymentIntent.status === "succeeded") {
+            console.log('payment intend', paymentIntent)
+            setTransectionId(paymentIntent.id)
+            setCardSuccess('Congratulation! Your payment is successfully done.')
+        } else {
+            setCardSuccess('');
+        }
+        setProcessing(false)
+    }
     return (
         <div>
+            <form onSubmit={handleSubmit}>
+                <CardElement
+                    options={{
+                        style: {
+                            base: {
+                                fontSize: '16px',
+                                color: '#424770',
+                                '::placeholder': {
+                                    color: '#aab7c4',
+                                },
+                            },
+                            invalid: {
+                                color: '#9e2146',
+                            },
+                        },
+                    }}
+                />
 
+                <button type="submit" disabled={!stripe} className='btn btn-primary btn-sm mt-6'>
+                    Pay
+                </button>
+            </form>
+            <p className='text-red-500'>{cardError}</p>
+            <p className='text-success'>{cardSuccess}</p>
         </div>
     );
 };
--- src/pages/Dashboard/Payment/Payment.js
+++ src/pages/Dashboard/Payment/Payment.js
@@ -8,14 +8,13 @@ const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PK);
 
 const Payment = () => {
     const booking = useLoaderData();
-    console.log(booking)
     const { productName, price, bookingDate, buyer, email, phone } = booking;
     return (
         <div className='text-center'>
             <h3 className="text-3xl">Payment for <span className='font-bold'>{productName}</span></h3>
             <p className='text-xl'>Please pay <span className='font-bold'>${price}</span> for purchase this product.</p>
 
-            <div className='w-96 my-12'>
+            <div className='w-96 my-12 mx-auto px-5 pt-8 pb-4 rounded-lg'>
                 <Elements stripe={stripePromise}>
                     <CheckoutForm
                         booking={booking}
