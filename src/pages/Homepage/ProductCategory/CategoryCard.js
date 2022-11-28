9eb5fbed3b5370dcdb1385dee77c5c5242689ebb
Rubayet-billah <rubayetbillah909@gmail.com>
2022-11-28 23:51:35 +0600

    slight change

--- src/pages/Homepage/ProductCategory/CategoryCard.js
+++ src/pages/Homepage/ProductCategory/CategoryCard.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const CategoryCard = ({ category }) => {
-    const { category: name, categoryId, image } = category;
+    const { category: name, categoryId, image, info } = category;
     return (
         <div className="card w-full bg-base-100 shadow-xl">
             <figure><img src={image} alt="Shoes" /></figure>
             <div className="card-body">
                 <h2 className="card-title">{name}</h2>
-                <p>Laptop between 10000 to 200000</p>
+                <p>{info}</p>
                 <div className="card-actions justify-end">
                     <Link to={`/products/${categoryId}`}><button className='btn btn-sm btn-primary'>All Products</button></Link>
                 </div>
