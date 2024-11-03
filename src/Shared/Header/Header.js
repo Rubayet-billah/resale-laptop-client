a7e94633d6328fa8a49713cc066b73ad7b3bfb4e
Rubayet-billah <rubayetbillah909@gmail.com>
2024-11-04 00:16:51 +0600

    navbar refactored

--- src/Shared/Header/Header.js
+++ src/Shared/Header/Header.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { AuthContext } from "../../contexts/AuthProvider/AuthProvider";
 import useRole from "../../Hooks/useRole/useRole";
@@ -7,7 +7,7 @@ import logo from "../../assets/logo.png";
 
 const Header = () => {
   const { user, roleState, logoutUser } = useContext(AuthContext);
-  const [role, roleLoader] = useRole(user?.email, roleState);
+  const [role] = useRole(user?.email, roleState);
   const navigate = useNavigate();
 
   const handleLogout = () => {
@@ -18,71 +18,10 @@ const Header = () => {
       .catch((err) => console.error(err));
   };
 
-  const menuItems = (
-    <>
-      <li>
-        <Link to="/" className="hover:text-accent">
-          Home
-        </Link>
-      </li>
-      <li>
-        <Link to="/blog" className="hover:text-accent">
-          Blog
-        </Link>
-      </li>
-      {user?.uid ? (
-        <>
-          {role === "Customer" && (
-            <li>
-              <Link to="/myorders" className="hover:text-accent">
-                My Orders
-              </Link>
-            </li>
-          )}
-          {role === "Seller" && (
-            <>
-              <li>
-                <Link to="/myproducts" className="hover:text-accent">
-                  My Products
-                </Link>
-              </li>
-              <li>
-                <Link to="/addproduct" className="hover:text-accent">
-                  Add A Product
-                </Link>
-              </li>
-              <li>
-                <Link className="hover:text-accent">My Buyers</Link>
-              </li>
-            </>
-          )}
-          {role === "Admin" && (
-            <>
-              <li>
-                <Link to="/dashboard/allsellers" className="hover:text-accent">
-                  All Sellers
-                </Link>
-              </li>
-              <li>
-                <Link
-                  to="/dashboard/allcustomers"
-                  className="hover:text-accent"
-                >
-                  All Customers
-                </Link>
-              </li>
-              <li>
-                <Link
-                  to="/dashboard/reportedproducts"
-                  className="hover:text-accent"
-                >
-                  Reported Products
-                </Link>
-              </li>
-            </>
-          )}
-        </>
-      ) : (
+  // Menu items based on role
+  const roleBasedLinks = () => {
+    if (!user?.uid) {
+      return (
         <>
           <li>
             <Link to="/login" className="hover:text-accent">
@@ -95,34 +34,105 @@ const Header = () => {
             </Link>
           </li>
         </>
-      )}
-    </>
-  );
+      );
+    }
+    switch (role) {
+      case "Customer":
+        return (
+          <li>
+            <Link to="/myorders" className="hover:text-accent">
+              My Orders
+            </Link>
+          </li>
+        );
+      case "Seller":
+        return (
+          <>
+            <li>
+              <Link to="/myproducts" className="hover:text-accent">
+                My Products
+              </Link>
+            </li>
+            <li>
+              <Link to="/addproduct" className="hover:text-accent">
+                Add A Product
+              </Link>
+            </li>
+            <li>
+              <Link to="/mybuyers" className="hover:text-accent">
+                My Buyers
+              </Link>
+            </li>
+          </>
+        );
+      case "Admin":
+        return (
+          <>
+            <li>
+              <Link to="/dashboard/allsellers" className="hover:text-accent">
+                All Sellers
+              </Link>
+            </li>
+            <li>
+              <Link to="/dashboard/allcustomers" className="hover:text-accent">
+                All Customers
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/dashboard/reportedproducts"
+                className="hover:text-accent"
+              >
+                Reported Products
+              </Link>
+            </li>
+          </>
+        );
+      default:
+        return null;
+    }
+  };
 
   return (
     <header className="sticky top-0 z-10 bg-white shadow-md">
       <CustomContainer>
-        {" "}
-        <nav className="px-4 py-4 navbar md:px-12">
+        <nav className="flex items-center justify-between px-4 py-4 md:px-12">
+          {/* Left: Logo */}
           <div className="navbar-start">
             <Link to="/">
-              {/* ReSeller */}
               <img className="w-24 lg:w-36 xl:w-44" src={logo} alt="logo" />
             </Link>
           </div>
-          <div className="hidden navbar-center lg:flex">
-            <ul className="space-x-4 menu menu-horizontal">{menuItems}</ul>
+
+          {/* Center: Menu items for larger screens */}
+          <div className="hidden lg:flex navbar-center">
+            <ul className="flex space-x-4 menu menu-horizontal">
+              <li>
+                <Link to="/" className="hover:text-accent">
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link to="/blog" className="hover:text-accent">
+                  Blog
+                </Link>
+              </li>
+              {roleBasedLinks()}
+            </ul>
           </div>
-          <div className="flex items-center space-x-4 navbar-end">
-            {user?.uid ? (
+
+          {/* Right: Logout button for authenticated users */}
+          <div className="flex justify-end navbar-end">
+            {user?.uid && (
               <button
                 onClick={handleLogout}
                 className="btn btn-sm btn-accent btn-outline"
               >
                 Logout
               </button>
-            ) : null}
+            )}
           </div>
+
           {/* Mobile Menu */}
           <div className="dropdown lg:hidden">
             <label tabIndex={0} className="btn btn-ghost">
@@ -143,9 +153,19 @@ const Header = () => {
             </label>
             <ul
               tabIndex={0}
-              className="p-2 space-y-2 bg-white shadow dropdown-content menu rounded-box w-52"
+              className="right-0 p-2 space-y-2 bg-white shadow dropdown-content menu rounded-box w-52"
             >
-              {menuItems}
+              <li>
+                <Link to="/" className="hover:text-accent">
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link to="/blog" className="hover:text-accent">
+                  Blog
+                </Link>
+              </li>
+              {roleBasedLinks()}
             </ul>
           </div>
         </nav>
